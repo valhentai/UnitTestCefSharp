7d4a3f892675e33b6f8beec5a2ae5e6eccb1c7e6
gael <gael.charpentier@utc.fr>
2018-02-07 10:33:29 +0100

    add browser creation

--- TestLibrary/Class1.cs
+++ TestLibrary/Class1.cs
@@ -14,6 +14,7 @@ namespace TestLibrary
         public void TestMethod1()
         {
             Cef.Initialize();
+            var web = new CefSharp.Wpf.ChromiumWebBrowser();
         }
     }
 }
